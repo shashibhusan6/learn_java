67a6dbaac2f106215cea298e0f57226432673cd5
shashibhusan6 <singhashashibhusan6@gmail.com>
2026-07-15 21:31:14 +0530

    Added medium level question answer

--- Ch_07_While_Question_Loops.java
+++ Ch_07_While_Question_Loops.java
@@ -386,3 +386,340 @@ class Example15 {
 
 
 
+/*
+=========================================
+           MEDIUM LEVEL
+=========================================
+*/
+
+
+// Q16. Find the sum of numbers from 1 to 10.
+
+class Example16 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+
+// Q17. Find the sum of numbers from 1 to 100.
+
+class Example17 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 100) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+
+// Q18. Find the sum of even numbers from 1 to 100.
+
+class Example18 {
+    public static void main(String[] args) {
+
+        int i = 2;
+        int sum = 0;
+
+        while (i <= 100) {
+            sum += i;
+            i += 2;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+
+// Q19. Find the sum of odd numbers from 1 to 100.
+
+class Example19 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 100) {
+            sum += i;
+            i += 2;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+
+// Q20. Find the factorial of a number.
+
+class Example20 {
+    public static void main(String[] args) {
+
+        int num = 5;
+        int i = 1;
+        int fact = 1;
+
+        while (i <= num) {
+            fact *= i;
+            i++;
+        }
+
+        System.out.println("Factorial = " + fact);
+
+    }
+}
+
+
+// Q21. Reverse the digits of a number.
+
+class Example21 {
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reverse = " + reverse);
+
+    }
+}
+
+
+// Q22. Count the number of digits in a number.
+
+class Example22 {
+    public static void main(String[] args) {
+
+        int num = 987654;
+        int count = 0;
+
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+
+        System.out.println("Digits = " + count);
+
+    }
+}
+
+
+// Q23. Find the sum of digits of a number.
+
+class Example23 {
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+
+// Q24. Check whether a number is a palindrome.
+
+class Example24 {
+    public static void main(String[] args) {
+
+        int num = 121;
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
+
+
+// Q25. Print the Fibonacci series up to N terms.
+
+class Example25 {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int a = 0;
+        int b = 1;
+        int count = 1;
+
+        while (count <= n) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+
+            count++;
+        }
+
+    }
+}
+
+
+// Q26. Find the largest digit in a number.
+
+class Example26 {
+    public static void main(String[] args) {
+
+        int num = 583921;
+        int largest = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit > largest) {
+                largest = digit;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println("Largest Digit = " + largest);
+
+    }
+}
+
+
+// Q27. Find the smallest digit in a number.
+
+class Example27 {
+    public static void main(String[] args) {
+
+        int num = 583921;
+        int smallest = 9;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit < smallest) {
+                smallest = digit;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println("Smallest Digit = " + smallest);
+
+    }
+}
+
+
+// Q28. Count even and odd digits in a number.
+
+class Example28 {
+    public static void main(String[] args) {
+
+        int num = 123456789;
+        int even = 0;
+        int odd = 0;
+
+        while (num > 0) {
+
+            int digit = num % 10;
+
+            if (digit % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println("Even Digits = " + even);
+        System.out.println("Odd Digits = " + odd);
+
+    }
+}
+
+
+// Q29. Print all factors of a number.
+
+class Example29 {
+    public static void main(String[] args) {
+
+        int num = 24;
+        int i = 1;
+
+        while (i <= num) {
+
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+
+    }
+}
+
+
+// Q30. Check whether a number is a prime number.
+
+class Example30 {
+    public static void main(String[] args) {
+
+        int num = 17;
+        int i = 2;
+        boolean isPrime = true;
+
+        while (i < num) {
+
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+
+            i++;
+        }
+
+        if (num <= 1) {
+            System.out.println("Not Prime");
+        } else if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+    }
+}
